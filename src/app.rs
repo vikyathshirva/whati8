ce500ceb9ac9e8ab612f609b480fad52a27bba55
Vikyath Shirva <vicky.fames@gmail.com>
2024-01-31 17:25:21 +0530

    PHASE1-adding participants lister
    PHASE1-

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,94 @@
 use leptos::{ev::SubmitEvent, html::Input, *};
 use leptos_meta::*;
 use leptos_router::*;
+use uuid::Uuid;
+use serde::{Deserialize, Serialize};
+use web_sys::console::log;
+
+
+
+
+const LINE_ITEM_STORAGE_KEY: &str = "line-item-storage";
+const PARTICIPANTS_STORAGE_KEY: &str  = "participants-storage";
+const EVENT_STORAGE_KEY: &str  = "event-item-storage";
+
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Participants(pub Vec<Participant>);
+
+impl Participants {
+    pub fn new() -> Self {
+        let starting_participants =
+            window()
+                .local_storage()
+                .ok()
+                .flatten()
+                .and_then(|storage| {
+                    storage.get_item(PARTICIPANTS_STORAGE_KEY).ok().flatten().and_then(
+                        |value| serde_json::from_str::<Vec<Participant>>(&value).ok(),
+                    )
+                })
+            .unwrap_or_default();
+
+        Self(starting_participants)
+    }
+
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn add(&mut self, participant: Participant) {
+        self.0.push(participant);
+    }
+
+    pub fn remove(&mut self, id: Uuid) {
+        self.retain(|participant| participant.id != id)
+    }
+
+
+    fn retain(&mut self, mut f: impl FnMut(&Participant) -> bool) {
+        self.0.retain(|participant| {
+            let retain = f(participant);
+            if !retain {
+                participant.name.dispose();
+            }
+            retain
+        })
+
+    }
+}
+
+
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Participant {
+    pub id: Uuid,
+    pub name: RwSignal<String>,
+}
+
+
+impl Participant {
+    pub fn new(id: Uuid, name: String) -> Self {
+        let name = create_rw_signal(name);
+        Self {
+            id,
+            name
+        }
+    }
+}
+
+
+
+
 
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+
+
+
 
     view! {
         // injects a stylesheet into the document <head>
@@ -29,21 +112,92 @@ pub fn App() -> impl IntoView {
     }
 }
 
-
 #[component]
 fn Homepage() -> impl IntoView {
-    let (eventName, setEventName) = create_signal(String::new());
-    let (eventDate, setEventDate) = create_signal(String::new());
-    let (participants, setParticipants) = create_signal(Vec<String>::new());
-    let (items, setItems) = create_signal(Vec::<(String, String, Vec<String>)>::new());
-    let (additionalTax, setAdditionalTax) = create_signal(String::new());
-    let (selectedParticipants, setSelectedParticipants) = create_signal(Vec::new());
+
+
+
+
 
     view! {
+        <body class="bg-gray-100">
+            <main class="container mx-auto p-4">
+                <section class="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto mt-8">
+                    <div class="mb-4">
+                        <div>Whati8 phase-1</div>
+                        <input type="text" id="e-name" placeholder="Enter the event name.." class="mt-2 p-2 border rounded-md w-full" />
+                    </div>
+                    <label for="e-date">Enter event date</label>
+                    <input type="date" id="e-date" class="mt-2 p-2 border rounded-md w-full" />
+                    <label for="e-tax">Enter tax amount</label>
+                    <input type="double" id="e-tax" placeholder="Enter the tax amount" class="mt-2 p-2 border rounded-md w-full" />
+                    <label for="e-amount">Enter total amount</label>
+                    <input type="double" id="e-amount" placeholder="Enter the total amount" class="mt-2 p-2 border rounded-md w-full" />
+                    <ParticipantAdder/>
+                </section>
+            </main>
+        </body>
     }
+}
 
+
+
+#[component]
+fn ParticipantAdder() -> impl IntoView {
+
+    let (participants, set_participants) = create_signal(Participants::new());
+
+
+    create_effect(move|_| {
+        if let Ok(Some(storage)) = window().local_storage() {
+            let json = serde_json::to_string(&participants)
+                .expect("Couldn't serialize participants");
+            if storage.set_item(PARTICIPANTS_STORAGE_KEY, &json).is_err() {
+                log::error!("Error while trying to set item in local storage")
+            }
+        }
+    });
+
+    let participant_input_ref = create_node_ref::<Input>();
+    let add_participant = move || {
+        let input = participant_input_ref.get().unwrap();
+        let name = input.value();
+        let name = name.trim();
+        if !name.is_empty() {
+            let new = Participant::new(
+                Uuid::new_v4(),
+                name.to_string()
+            );
+            set_participants.update(|p| p.add(new));
+            input.set_value("");
+        }
+    };
+
+    let all_participants = move|| {
+        participants.with(|participant| participant.0.to_vec())
+    };
+
+
+    let participants = vec!["Participant 1", "Participant 2", "Participant 3"];
+    view! {
+    <label for="participants">add participants </label>
+    <input type="text" name="participants"></input>
+    <button id="addButton">Add</button>
+    {all_participants().iter()
+            .map(|n| view!{
+                <div >{n.name.get().to_string()}</div>
+            })
+            .collect_view()}
+
+
+    <select name="participants" id="participants-selector" class="mt-2 p-2 border rounded-md w-full" >
+    </select>
+}
 
 }
+
+
+
 
 
 /// 404 - Not Found
